bbaef4d8a6db2c7c313302239396a22fb96a7b1a
Gnanasree-2400032296 <2400032296@kluniversity.in>
2026-01-19 19:30:23 +0530

    Add DI-Cr-Annotation project

---
+++ DI-Cr-Annotation/src/main/java/com/klu/demo/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.klu.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.klu.demo.model")
+public class AppConfig {
+
+}
